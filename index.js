cb8c169e36853cb437df4ab5fbc9ee4b80421660
Patricia Tirolla <patricia.tirolla@hyperisland.se>
2024-10-09 17:35:29 +0200

    Step 4 - Add "new book" button

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 const myLibrary = [];
 
-// Add book to library
-function addBookToLibrary() {
-    let newBookTitle = prompt("Add the book title");
-    let newBookAuthor = prompt("Add the author");
-    let newBookPage = prompt("Add the number of pages");
-    const book = new Book(newBookTitle, newBookAuthor, newBookPage);
+// // Add book to library
+// function addBookToLibrary() {
+//     let newBookTitle = prompt("Add the book title");
+//     let newBookAuthor = prompt("Add the author");
+//     let newBookPage = prompt("Add the number of pages");
+//     const book = new Book(newBookTitle, newBookAuthor, newBookPage);
 
+//     myLibrary.push(book);
+//     return myLibrary;
+// }
+
+function addBookToLibrary() {
+    let bookTitle = document.getElementById("book-title");
+    let bookTitleValue = bookTitle.value;
+
+    let bookAuthor = document.getElementById("author");
+    let bookAuthorValue = bookAuthor.value;
+
+    let bookPage = document.getElementById("pages");
+    let bookPageValue = bookPage.value;
+
+    const book = new Book(bookTitleValue, bookAuthorValue, bookPageValue);
 
     myLibrary.push(book);
-    return myLibrary;
 }
-addBookToLibrary();
-console.log(myLibrary);
+
 
 // Display each book on the page
 function displayEachBook() {
@@ -30,3 +43,25 @@ function Book(title, author, pages) {
     }
 }
 
+
+// connect form buttons
+const openAddNewBookButton = document.querySelector("[data-open-modal]");
+const closeButton = document.querySelector("[data-close-modal]");
+const modal = document.querySelector("[data-modal]");
+const submitForm = document.querySelector("#library-book-registration")
+
+openAddNewBookButton.addEventListener("click", () => {
+    modal.show();
+})
+
+// closeButton.addEventListener("click", () => {
+//     modal.close()
+// })
+
+submitForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    addBookToLibrary();
+    modal.close();
+
+})
+console.log(myLibrary);
