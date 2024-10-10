bd876f499946eb599e511025bb89b40bdcaacc4a
Patricia Tirolla <patricia.tirolla@hyperisland.se>
2024-10-10 17:31:04 +0200

    Step 6 - Read status checkbox

--- index.js
+++ index.js
@@ -12,16 +12,17 @@ function addBookToLibrary() {
     let bookPage = document.getElementById("pages");
     let bookPageValue = bookPage.value;
 
-    const book = new Book(bookTitleValue, bookAuthorValue, bookPageValue);
+    const book = new Book(bookTitleValue, bookAuthorValue, bookPageValue, false);
 
     myLibrary.push(book);
 }
 
 // The constructor
-function Book(title, author, pages) {
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.read = read;
     this.info = function() {
        return console.log(this.title + this.author + this.pages)
     }
@@ -29,44 +30,82 @@ function Book(title, author, pages) {
 
 // Display the books in a table
 function createTable() {
+    let display = document.getElementById("display");
+    display.textContent = "";
 
     // Select each book in the list and create a new column
-    let display = document.getElementById("display");
     myLibrary.forEach((book, index) => {
 
+        // table row
         let tableRow = document.createElement("tr");
         tableRow.setAttribute("data-index", index);
         display.appendChild(tableRow);
 
-        let tableColumn1 = document.createElement("td");
-        let tableColumn2 = document.createElement("td");
-        let tableColumn3 = document.createElement("td");
-        
-        tableRow.appendChild(tableColumn1);
-        tableRow.appendChild(tableColumn2);
-        tableRow.appendChild(tableColumn3);
+        // title column
+        let titleColumn = document.createElement("td");
+        tableRow.appendChild(titleColumn);
+        titleColumn.textContent = book.title;
 
-        tableColumn1.textContent = book.title;
-        tableColumn2.textContent = book.author;
-        tableColumn3.textContent = book.pages;
+        // author column
+        let authorColumn = document.createElement("td");
+        tableRow.appendChild(authorColumn);
+        authorColumn.textContent = book.author;
 
+        // pages column
+        let pagesColumn = document.createElement("td");
+        tableRow.appendChild(pagesColumn);
+        pagesColumn.textContent = book.pages;
+
+        // read column
+        let readColumn = document.createElement("td");
+        let checkboxInput = document.createElement("input");
+        let checkboxLabel = document.createElement("label");
+        tableRow.appendChild(readColumn);
+        readColumn.appendChild(checkboxInput);
+        readColumn.appendChild(checkboxLabel);
+
+        // set checkbox attribute
+        checkboxInput.setAttribute("type", "checkbox");
+        checkboxInput.id = "read-checkbox-" + index;
+        checkboxLabel.textContent = "Read";
+        checkboxLabel.htmlFor = "read-checkbox-" + index;
+
+        // Check if the book is read or not
+        if (book.read === true) {
+            checkboxInput.checked = true;
+        } else {
+            checkboxInput.checked = false;
+        }
+
+        // Change the book read status when click the checkbox
+        checkboxInput.addEventListener("click", () => {
+            if (checkboxInput.checked === true) {
+                book.read = true;
+            } else {
+                book.read = false;
+            }
+        })
+
+        // delete column
+        let deleteColumn = document.createElement("td");
+        tableRow.appendChild(deleteColumn);
+
+        // Remove button
         const removeButton = document.createElement("button");
         removeButton.classList.add("remove-button")
         removeButton.textContent = ("Delete");
-        tableRow.appendChild(removeButton);
+        deleteColumn.appendChild(removeButton);
 
         removeButton.addEventListener("click", () => {
+            myLibrary.splice(index, 1);
             tableRow.remove();
         })
-
-        
-    });            
+    })       
 }
-
 
 // connect form buttons 
 const openNewBookButton = document.querySelector("#new-book-button");
-const cancelButton = document.getElementById("cancel-button");
+const cancelButton = document.querySelector("#cancel-button");
 
 openNewBookButton.addEventListener("click", () => {
     modal.show();
@@ -93,3 +132,5 @@ submitForm.addEventListener("submit", (e) => {
     // the .reset() method clears the form when I click "submit"
     submitForm.reset()
 })
+
+// Read read
